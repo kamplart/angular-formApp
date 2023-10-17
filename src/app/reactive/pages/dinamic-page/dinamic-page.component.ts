2e2dbb3666b90f7db4bb41e62c11a997bcc3406f
CamiloAir <dev1@e-advising.biz>
2023-10-16 19:42:11 -0500

    Rutas

---
+++ src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dinamic-page',
+  templateUrl: './dinamic-page.component.html',
+  styles: [
+  ]
+})
+export class DinamicPageComponent {
+
+}
--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DinamicPageComponent } from './pages/dinamic-page/dinamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children:[
+      {path:'basic', component: BasicPageComponent},
+      {path:'dinamic', component: DinamicPageComponent},
+      {path:'switches', component: SwitchesPageComponent},
+      {path:'**', redirectTo: 'basic'},
+
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
